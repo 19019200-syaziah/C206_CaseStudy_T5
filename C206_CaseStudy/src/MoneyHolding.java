3c295141ff2fb988787569fba47a0f680bf4b77a
19006319 <19006319@DESKTOP-DIMLQ26>
2020-08-21 10:04:48 +0800

    update MoneyHolding

--- C206_CaseStudy/src/MoneyHolding.java
+++ C206_CaseStudy/src/MoneyHolding.java
@@ -1,4 +1,27 @@
 
 public class MoneyHolding {
+	
+	private String iso;
+	private double holdingAmt;
+	
+	public MoneyHolding(String iso, double holdingAmt) {
+		this.iso = iso;
+		this.holdingAmt = holdingAmt;
+	}
+	
+	public String getIso() {
+		return iso;
+	}
+	public double getHoldingAmt() {
+		return holdingAmt;
+	}
+	public String toString() {
+		String output = "";
+		output = String.format("%-10s %-10s %10s %10d\n", getIso(),getHoldingAmt());
+		return output;
+	}
+	
 
 }
+
+	
